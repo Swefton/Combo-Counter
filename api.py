17f707ffc223a76fc5aaef66a62a517ef2b6ed0e
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-03-17 20:05:21 -0400

    Cleaning up imports

--- api.py
+++ api.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.edge.service import Service
 
